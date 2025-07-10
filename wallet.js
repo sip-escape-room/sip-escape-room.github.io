9ed5152674741fb4480751b29bb82ab69db80d80
sip-escape-room <synkvelvet@gmail.com>
2025-07-11 05:05:01 +0800

    edit

--- wallet.js
+++ wallet.js
@@ -1,5 +1,17 @@
 //import { ans1, ans2, ans3, ans4, ans5, ans6, ans7, ans8, ans9, ans10, ans11, ans12 } from './answers.js';
-import { ans1 } from './answers.js';
+
+const ans1 = 'one';
+const ans2 = 'two';
+const ans3 = 'three';
+const ans4 = 'four';
+const ans5 = 'five';
+const ans6 = 'six';
+const ans7 = 'seven';
+const ans8 = 'eight';
+const ans9 = 'nine';
+const ans10 = 'ten';
+const ans11 = 'eleven';
+const ans12 = 'twelve';
 
 function onSubmit() {
 
